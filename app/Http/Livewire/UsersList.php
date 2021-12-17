8e71be8ba383b478f0330d29c5d6fd801488c58e
Agujas1993 <samuel.smr2.local@gmail.com>
2021-12-17 21:16:53 +0100

    Se puede filtrar por estado, rol y skills con livewire

--- app/Http/Livewire/UsersList.php
+++ app/Http/Livewire/UsersList.php
@@ -14,9 +14,15 @@ class UsersList extends Component
     public $view;
     public $originalUrl;
     public $search;
+    public $state;
+    public $role;
+    public $skills = [];
 
     protected $queryString = [
-        'search' => ['except' => '']
+        'search' => ['except' => ''],
+        'state' => ['except' => 'all'],
+        'role' => ['except' => 'all'],
+        'skills' => [],
     ];
 
     public function mount($view, Request $request)
@@ -39,10 +45,10 @@ class UsersList extends Component
             })
             ->filterBy($userFilter, array_merge(
                 ['trashed' => request()->routeIs('users.trashed')],
-                ['state' => request('state'),
-                    'role' => request('role'),
+                ['state' => $this->state,
+                    'role' => $this->role,
                     'search' => $this->search,
-                    'skills' => request('skills'),
+                    'skills' => $this->skills,
                     'from' => request('from'),
                     'to' => request('to'),
                     'order' => request('order'),
@@ -67,7 +73,7 @@ class UsersList extends Component
             'users' => $this->getUsers($userFilter),
             'view' => $this->view,
             'sortable' => $sortable,
-            'skills' => Skill::getList(),
+            'skillsList' => Skill::getList(),
             'checkedSkills' => collect(request('skills')),
         ]);
     }
